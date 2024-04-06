c9a9233524baaa3f990d223f3f72da3e0bb2b677
Zolastic <honhattienlul@gmail.com>
2024-04-06 19:45:28 +0800

    feat: installed TailwindCSS and updated build process
    - Added TailwindCSS as a dev dependency
    - Created postcss.config.js with necessary plugins
    - Modified package.json scripts to include the CSS build steps
    - Ensured that the output CSS is the combination of Tailwind styles and custom styles

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,12 +1,12 @@
 import React from "react";
-import "./Button.css";
+import "../styles.css";
 
 type Props = {
   label: string;
 };
 
 const Button = ({ label }: Props) => {
-  return <button className="test">{label}</button>;
+  return <button className="bg-red-700 p-4 mx-2 rounded-lg">{label}</button>;
 };
 
 export default Button;
