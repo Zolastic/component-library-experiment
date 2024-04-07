6f626816b141e160868710d06342e780237a00e5
Zolastic <honhattienlul@gmail.com>
2024-04-07 23:10:28 +0800

    refactor: Remove unnecessary comments

--- src/components/Select/Select.tsx
+++ src/components/Select/Select.tsx
@@ -173,13 +173,3 @@ export {
   SelectScrollUpButton,
   SelectScrollDownButton,
 };
-
-// import React from "react";
-
-// type Props = {};
-
-// const Select = (props: Props) => {
-//   return <div>Select</div>;
-// };
-
-// export default Select;
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,2 @@
 export { default as Button } from "./Button";
-// export { default as Select } from "./Select";
 export * from "./Select";
