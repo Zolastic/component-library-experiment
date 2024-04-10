e7b4703eea5c0f95da9463022f2e869da6e65ee8
Zolastic <honhattienlul@gmail.com>
2024-04-10 17:55:49 +0800

    feat: working on improved MultiSelect component

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,5 @@
 export { default as Button } from "./Button";
 export * from "./Select";
 export { default as MultiSelect } from "./MultiSelect";
+export * from "./MultipleSelect";
 export * from "./Badge";
